cc6c96f82d19db73f3e11f9c585bf512d4bd7137
Redson Eric <redsoneric82@gmail.com>
2024-06-13 14:36:35 +0300

    New features Add Ennemi
    This version adds some enemies, but they don't move yet ;)

--- Game/main.py
+++ Game/main.py
@@ -2,42 +2,73 @@ import pygame as p
 from time import sleep
 from random import randint
 from player import Player
+from ennemi import Bot
 
 class Game:
-    def __init__(self,titre):
+    def __init__(self):
         self.fps = p.time.Clock()
-
+        p.display.set_caption("Simple 2d Game")
         self.state = 1
-        p.display.set_caption(titre)
+        self.manche = 1
+        self.ennemiList = []
         self.screen = p.display.set_mode((720,480))
-        self.player = Player("Me")
-
+        self.player = Player()
     def keyEvent(self):
+        limitWidth = self.screen.get_width()
+        limitHeight = self.screen.get_height()
         keys = p.key.get_pressed()
         #### Y
-        if keys[p.K_z]:
+        playerTop = self.player.hitBox.top
+        playerLeft = self.player.hitBox.left
+        playerRight = self.player.hitBox.right
+        playerBottom = self.player.hitBox.bottom
+
+        if keys[p.K_z] and playerTop>0:
             self.player.velocityY = -1
-        elif keys[p.K_s]:
+        elif keys[p.K_s] and playerBottom<limitHeight:
             self.player.velocityY = 1
         else:
             self.player.velocityY = 0
         #########X
-        if keys[p.K_q]:
+        if keys[p.K_q] and playerLeft>0:
             self.player.velocityX = -1
-        elif keys[p.K_d]:
+        elif keys[p.K_d] and playerRight<limitWidth:
             self.player.velocityX = 1
         else:
             self.player.velocityX = 0
+
+    def spawnBot(self,n):
+        width = self.screen.get_width()
+        height = self.screen.get_height()
+        ### bot making
+        nbrEnnemi = len(self.ennemiList);
+        if nbrEnnemi <= 0:
+            for x in range(n):
+                randomX = randint(20, width - 20)
+                randomY = randint(20, height - 20)
+                n = Bot()
+                n.hitBox.centerx = randomX
+                n.hitBox.centery = randomY
+                self.ennemiList.append(n)
+        ### bot spawning
+        else:
+            for bot in self.ennemiList:
+                bot.draw(self.screen)
+
+
+
 
     def drawScreen(self):
         self.screen.fill((0,0,0))
         self.player.draw(self.screen)
         self.player.move()
+        self.spawnBot(5)
 
 
 
     def updateScreen(self):
         p.display.flip()
+        self.fps.tick(60)
 
     def start(self):
         while self.state == 1:
@@ -46,13 +77,13 @@ class Game:
                     self.state = 0
                 else:
                     continue
-            self.fps.tick(60)
+
             self.keyEvent()
             self.drawScreen()
             self.updateScreen()
 
 
-game = Game("My 2d game")
+game = Game()
 game.start()
 
 p.quit()
--- Game/player.py
+++ Game/player.py
@@ -1,14 +1,13 @@
 import pygame as p
 
 class Player:
-    def __init__(self,name):
-        self.name = name
+    def __init__(self):
         self.image = p.image.load("resource/player.png")
         self.hitBox = self.image.get_rect()
         self.velocityX = 0
         self.velocityY = 0
         self.hp = 100
-        self.speed = 2
+        self.speed = 3
 
     def draw(self, surface):
         surface.blit(self.image, self.hitBox)
