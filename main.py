62282185b1a37f90bb69d5027db417ce906e1dde
unknown <rafihobiananyavo@gmail.com>
2024-06-26 17:04:27 +0100

    Fix player orientation

--- main.py
+++ main.py
@@ -1,46 +1,74 @@
 import pygame
 import pygame as p
-import keyboard as k
 from time import sleep
 from random import randint
 from player import Player
 from ennemi import Bot
 import math
+
 class Game:
+
     def __init__(self):
+        """Setup Game Environments"""
         self.fps = p.time.Clock()
         p.display.set_caption("Simple 2d Game")
+
+        # a boolean running state 
         self.state = 1
+
+        # setup crosshair
         self.crosshair = p.image.load("resource/cr.png")
         self.crosshairBox = self.crosshair.get_rect()
+
         self.manche = 1
+
+        # setup enemi
         self.ennemiList = []
         self.ennemiHitBox = []
+
+        # mouse coordinates
         self.mouseX = 0
         self.mouseY = 0
+
         self.screen = p.display.set_mode((720,480))
+
+        # setup dead's screen
         self.deadScreen = p.image.load("resource/dead.png")
         self.deadScreenRect = self.deadScreen.get_rect()
+
+        # create player in the center of the screen
         self.player = Player()
-        self.player.hitBox.centerx = 720/2
-        self.player.hitBox.centery = 480/2
+        self.player.hitBox.centerx = self.screen.get_width()/2
+        self.player.hitBox.centery = self.screen.get_height()/2
+
     def keyEvent(self):
+        """
+        Control player directions
+        and Game options (quit and restart)
+        """
+
+        # Is player actually under attack or not
+        # Play sound if that is the case
         self.player.doDammage(self.ennemiHitBox)
+        
+        # Get current screen size
         limitWidth = self.screen.get_width()
         limitHeight = self.screen.get_height()
+
         keys = p.key.get_pressed()
-        #### Y
         playerTop = self.player.hitBox.top
         playerLeft = self.player.hitBox.left
         playerRight = self.player.hitBox.right
         playerBottom = self.player.hitBox.bottom
 
+        #### Y
         if keys[p.K_z] and playerTop>0:
             self.player.velocityY = -1
         elif keys[p.K_s] and playerBottom<limitHeight:
             self.player.velocityY = 1
         else:
             self.player.velocityY = 0
+
         #########X
         if keys[p.K_q] and playerLeft>0:
             self.player.velocityX = -1
@@ -48,11 +76,14 @@ class Game:
             self.player.velocityX = 1
         else:
             self.player.velocityX = 0
-        #############"
+
+        # Game options
         if keys[p.K_r] and self.player.isAlive == False:
+            # Restart
             print("restart")
             self.restart()
         elif keys[p.K_ESCAPE]:
+            # Leave Game
             self.state = 0
 
     def spawnBot(self,n):
@@ -65,6 +96,7 @@ class Game:
             for x in range(n):
                 randomX = randint(0, width)
                 randomY = randint(-20, 0) or randint(480, 500)
+                
                 n = Bot()
                 n.hitBox.centerx = randomX
                 n.hitBox.centery = randomY
@@ -104,15 +136,25 @@ class Game:
         self.player.hitBox.centerx = 720/2
         self.player.hitBox.centery = 480/2
 
-
     def drawScreen(self):
+        """
+        Draw game objects in the screen.
+        And update player and bots
+        """
+
+        # Draw player
         self.screen.fill((0,0,0))
         self.player.draw(self.screen)
+        self.screen.blit(self.crosshair, self.crosshairBox)
+
+        # Update player and attempt to spawn or move bots
         self.player.move()
         self.spawnBot(8)
-        self.screen.blit(self.crosshair, self.crosshairBox)
+
+        # Draw dead screen when player deads
         if (self.player.isAlive == False):
             self.screen.blit(self.deadScreen, self.deadScreenRect)
+            # Delete bots
             self.deleteAllBot()
 
     def getRotateAngle(self, x, y):
@@ -120,21 +162,30 @@ class Game:
         angleDegree = angleRadian * (180/math.pi)
         angleDegree *= -1
         return angleDegree
+    
     def updateScreen(self):
-
+        """Update: screen and player angle"""
         p.display.flip()
         self.mouseX = p.mouse.get_pos()[0]
         self.mouseY = p.mouse.get_pos()[1]
+
+        # update crosshair positions
         self.crosshairBox.centerx = self.mouseX
         self.crosshairBox.centery = self.mouseY
+
         self.player.angle = self.getRotateAngle(self.mouseX - self.player.hitBox.centerx, self.mouseY - self.player.hitBox.centery)
-        print(f"L angle est : {self.player.angle}")
+        print(f"L angle est : {self.player.angle}") # FIXME: This is just for player angle debugging
 
         p.mouse.set_visible(False)
 
         self.fps.tick(60)
 
     def start(self):
+        """
+        Starting point in this game.
+        It is the main loop for which we listen for events and its response,
+        and the main update.
+        """
         while self.state == 1:
             for ev in p.event.get():
                 if ev.type == p.QUIT:
--- player.py
+++ player.py
@@ -3,34 +3,56 @@ import pygame as p
 class Player:
     def __init__(self):
         self.image = p.image.load("resource/player.png")
-        self.hitBox = self.image.get_rect()
+        self.hitBox = self.image.get_rect() # Player position
         self.soundHurt = p.mixer.Sound("sound/playerHurt.mp3")
+
+        # Direction vectors
         self.velocityX = 0
         self.velocityY = 0
-        self.hp = 20
+        
+        self.hp = 20    # Player health
         self.speed = 3
-        self.angle = 0.0
         self.isAlive = True
+        self._angle = 0.0
 
     def draw(self, surface):
         #rotateImage = p.transform.rotate(self.image, 90)
         #self.image = rotateImage
-        surface.blit(self.image, self.hitBox)
+        rotated_image = p.transform.rotate(self.image, self.angle)
+        surface.blit(rotated_image, self.hitBox)
+
 
     def checkIfAlive(self):
         if self.hp <=0:
             self.isAlive = False
         else:
             pass
+
     def doDammage(self, ennemiList):
+        # FIXME: The next code is currently useless
         self.checkIfAlive()
+
         for x in ennemiList:
             if self.hitBox.colliderect(x):
                 self.soundHurt.play()
-                print(f"{self.hp}")
+                print(f"{self.hp}") # debuging for player health ????
             else:
                 pass
 
     def move(self):
+        """Move player dependend on his directional vectors"""
         self.hitBox.move_ip(self.velocityX*self.speed, self.velocityY*self.speed)
+
+    # Getter and setter for player angle
+    @property
+    def angle(self): return self._angle
+
+    @angle.setter
+    def angle(self, degree: float):
+        """
+        Changle player image angle
+        by setting from the top center of 
+        an imaginary circle in the image(90deg)
+        """
+        self._angle = degree - 90.0
 
